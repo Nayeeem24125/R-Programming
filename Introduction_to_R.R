91148437fc26e840ea79740c5ba2f1b7d197d172
Nayeem <nayeem24125@gmail.com>
2025-05-22 22:46:41 +0600

    First Commit

---
+++ Introduction_to_R.R
@@ -0,0 +1,19 @@
+n=5L
+as.integer(n)
+is.numeric(n)
+city='pabna'
+is.character(city)
+is.integer(n)
+is.character(city)
+gender<-factor(c('male','Female','Female','male'))
+as.integer(gender)
+apple<-factor(c('yes','no','sometimes','sometimes','yes'))
+apple
+as.numeric(apple)
+as.integer(apple)
+age=5
+age=as.character(age)
+class(age)
+#arithmatic operation
+
+
